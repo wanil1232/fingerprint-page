e3f18b6affddd90048d11a995e5fda9c61a31ba3
wanil1232 <frakiva61@gmail.com>
2025-05-09 13:40:38 +0300

    index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // מאפשר לכל מקור לגשת
-app.use((req, res, next) => {
+app.get('/fingerprint', async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   next();
 });
